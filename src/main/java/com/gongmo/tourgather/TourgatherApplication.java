92f76be40b3d1c21326c6879dd161898e15be972
yukong <kyb062666@gmail.com>
2024-04-08 17:21:06 +0900

    chore: 초기 프로젝트 셋팅

---
+++ src/main/java/com/gongmo/tourgather/TourgatherApplication.java
@@ -0,0 +1,13 @@
+package com.gongmo.tourgather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourgatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourgatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gongmo/tourgather/TourgatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gongmo.tourgather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourgatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
